920dfa19a408b14e77247ad89a66b6ee2d0163a1
ozdmrcht <ozdmrcht@gmail.com>
2023-03-28 18:12:44 -0500

    The basic operational

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+       solaySystem solar= new solarSystem();
+       solar.sun="1";
+       solar.moon="2";
+       solar.planets="3";
+    }
+}
